f599a89115595952e05363f86bd6ae860070dc0b
Ryan Propst <ryanpropst@gmail.com>
2019-10-29 09:52:46 -0500

    added some clairfy text

--- public/src/index.js
+++ public/src/index.js
@@ -3,4 +3,5 @@ import { render } from 'react-dom';
 
 import App from './components/App'
 
+// entry point into our React components
 render( <App value="React can be very confusing!" /> , document.getElementById('root'));
